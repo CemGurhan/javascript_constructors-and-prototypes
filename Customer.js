5061f80d7e750468685a3f06344ec11e0cc23e3b
Cem Gurhan <cemg@live.co.uk>
2022-06-01 15:28:08 +0100

    fisrt test ran and passed for customer

--- Customer.js
+++ Customer.js
@@ -1,3 +1,7 @@
+const Dealership = require("./Dealership");
+
+
+
 class Customer{
 
     constructor(name,wallet,car){
@@ -6,6 +10,23 @@ class Customer{
         this.car = null;
     }
 
+    buyCar(carIn){
 
+
+        if(this.wallet >= carIn.price){
+
+            this.car = carIn;
+            this.wallet -= carIn.price;
+
+            // Dealership.cars_in_stock.splice(cars_in_stock.indexOf(carIn),1);
+
+        }
+
+
+    }
+
+    
 
 }
+
+module.exports = Customer;
